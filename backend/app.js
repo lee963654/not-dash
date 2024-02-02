abb05615c9403d54269c129b3507dbda8b419cc5
James Lee <lee963654@gmail.com>
2024-02-02 10:15:01 -0500

    catch unhandled req and forawrd to error handler

--- backend/app.js
+++ backend/app.js
@@ -42,6 +42,17 @@ if (!isProduction) {
     })
   );
 
+  // Connect all the routes
   app.use(routes)
+
+  // Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
 
 module.exports = app
