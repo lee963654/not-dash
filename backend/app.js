483d09e02d26af0cd9eddf24964f051f7918aa08
James Lee <lee963654@gmail.com>
2024-02-02 10:16:05 -0500

    process sequelize errors

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes')
 
+const { ValidationError } = require('sequelize');
+
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -51,6 +53,20 @@ app.use((_req, _res, next) => {
   err.title = "Resource Not Found";
   err.errors = { message: "The requested resource couldn't be found." };
   err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
   next(err);
 });
 
