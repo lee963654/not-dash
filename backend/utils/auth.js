f09b7aa8cae46429887d5dc2a81d496f7864fa14
James Lee <lee963654@gmail.com>
2024-02-13 12:23:58 -0500

    added require auth

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -60,3 +60,14 @@ const setTokenCookie = (res, user) => {
       return next();
     });
   };
+
+// If there is no current user, return an error
+const requireAuth = function (req, _res, next) {
+    if (req.user) return next();
+
+    const err = new Error('Authentication required');
+    err.title = 'Authentication required';
+    err.errors = { message: 'Authentication required' };
+    err.status = 401;
+    return next(err);
+  }
