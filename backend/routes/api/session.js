d9bcceb9435488ce0486c43ce2d3ea65e60116bb
James Lee <lee963654@gmail.com>
2024-03-06 12:55:35 -0500

    added validation to session file

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,6 +3,9 @@ const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
